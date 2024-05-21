2114e1431eaa7aa57d692abb31fbbc043674a985
DungNBhehe <158248004+DungNBhehe@users.noreply.github.com>
2024-05-21 08:25:29 +0700

    layout

--- app/src/main/java/com/example/qlisinhvien/MainActivity.java
+++ app/src/main/java/com/example/qlisinhvien/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qlisinhvien;
 
+import android.annotation.SuppressLint;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -24,15 +25,29 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter myAdapter;
     SQLiteDatabase myDatabase;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtmalop = findViewById(R.id.edtMalop);
+        edttenlop = findViewById(R.id.edtTenlop);
+        edtsiso = findViewById(R.id.edtSiso);
+        BtnNhap = findViewById(R.id.BtnNhap);
+        BtnXoa = findViewById(R.id.BtnXoa);
+        BtnCapnhat = findViewById(R.id.BtnCapnhat);
+        BtnDulieu = findViewById(R.id.BtnDulieu);
+        //Tạo listview
+        lv = findViewById(R.id.lv);
+        myList = new ArrayList<>();
+        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,myList);
+        lv.setAdapter(myAdapter);
+        //Tạo và mở csdl
+        myDatabase = openOrCreateDatabase("qlisinhvien.db",MODE_PRIVATE,null);
+
+
+
         });
     }
 }
